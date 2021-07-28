7b9d6c1ae44ef902e84903c30dfb92ccf6a6eba1
bimnguyen <namnn9420@gmail.com>
2021-07-28 10:37:10 +0700

    nam lam cai ni ne

--- TemplateCuteBird.ViewModels/Catalog/Products/ProductCreateRequest.cs
+++ TemplateCuteBird.ViewModels/Catalog/Products/ProductCreateRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TemplateCuteBird.ViewModels.Catalog.Products
@@ -12,7 +13,8 @@ namespace TemplateCuteBird.ViewModels.Catalog.Products
         public string Detail { get; set; }
         public string Title { get; set; }
         public int Stock { get; set; }
-        public bool? IsFeatured { get; set; }
+        [Required(ErrorMessage = "Bạn phải nhập tên sản phẩm")]
+     
         public IFormFile ThumbnailImage { get; set; }
 
     }
--- TemplateCuteBird.WebApp/Controllers/SellerController.cs
+++ TemplateCuteBird.WebApp/Controllers/SellerController.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TemplateCuteBird.ApiIntegration;
+using TemplateCuteBird.ViewModels.Catalog.Products;
 
 namespace TemplateCuteBird.WebApp.Controllers
 {
     public class SellerController : Controller
     {
+        private readonly IProductApiClient _productApiClient;
+        private readonly IConfiguration _configuration;
+        private readonly ICategoryApiClient _categoryApiClient;
+        public SellerController(IProductApiClient productApiClient, IConfiguration configuration,
+     ICategoryApiClient categoryApiClient)
+        {
+            _productApiClient = productApiClient;
+            _configuration = configuration;
+            _categoryApiClient = categoryApiClient;
+        }
         public IActionResult Dashboard()
         {
             return View();
@@ -18,9 +31,30 @@ namespace TemplateCuteBird.WebApp.Controllers
             return View();
         }
 
-        public IActionResult CreateGig()
+    
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Create([FromForm] ProductCreateRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View(request);
+
+            var result = await _productApiClient.CreateProduct(request);
+            if (result)
+            {
+                TempData["result"] = "Add success";
+                return RedirectToAction("Dashboard");
+            }
+
+            ModelState.AddModelError("", "Add product failure");
+            return View(request);
+        }
     }
+
 }
